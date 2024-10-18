304f5830f2d2c3de88e10b2fb007f6cc09f918d5
mateonation <mattfernandezrivera@gmail.com>
2024-10-18 12:30:28 +0200

    all boxes have now shuffled colours

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 const total=12;
 let boxes=[];
+let randnum=['a','a','b','b','c','c','d','d','e','e','f','f'];
+const shufflear = randnum.sort((a, b) => 0.5 - Math.random());
 
-let boxtemp='<div class="box"><div class="hidden"></div><div class="front"></div></div>'
 
+// XERAR CAIXAS NO HTML
 for(let i=0;i<total;i++){
     let div=document.createElement('div');
-    div.innerHTML=boxtemp;
+    div.classList.add('box');
+    div.id=shufflear[i];
     boxes.push(div);
     document.querySelector('.boxes').append(boxes[i]);
 }
