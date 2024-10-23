7043d1e0a1e40ffbade1acbd5789d738f9f02f73
mateonation <mattfernandezrivera@gmail.com>
2024-10-23 08:55:57 +0200

    id randomizer and setter was added

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
 const total=12;
 let boxes=[];
-let randnum=['a','a','b','b','c','c','d','d','e','e','f','f'];
-const shufflear = randnum.sort((a, b) => 0.5 - Math.random());
+over=false;
 
+// ID posíbel
+let abc=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+let index=[];
+
+// Engadir tantos ID ao índice coma sexa posíbel (ata a metade das caixas totais)
+for(let i=0;i<total/2;i++){
+    index+=[abc[i]];
+}
+
+// Repetir outra vez o índice dentro de si mesmo e mesturar os elementos nel
+index=[...index,...index]
+const shuffled = index.sort((a, b) => 0.5 - Math.random());
 
 // XERAR CAIXAS NO HTML
 for(let i=0;i<total;i++){
     let div=document.createElement('div');
     div.classList.add('box');
-    div.id=shufflear[i];
+    div.addEventListener('click',selectbox);
+    div.id=shuffled[i];
     boxes.push(div);
     document.querySelector('.boxes').append(boxes[i]);
+}
+
+function selectbox(){
+    
 }
