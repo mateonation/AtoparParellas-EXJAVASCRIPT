b4d88a543509049dbc8d7d2788b257437b16237f
mateonation <mattfernandezrivera@gmail.com>
2024-10-24 09:07:36 +0200

    attempt/matches to find text are now functional

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ let boxes=[];
 let selboxes=[];
 let selid=[];
 let e=0;
+let attempts=0;
+let tofind=total/2;
 
 // ID posíbel
 let abc=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
@@ -26,6 +28,7 @@ for(let i=0;i<total;i++){
     div.id=i;
     boxes.push(div);
     document.querySelector('.boxes').append(boxes[i]);
+    attext();
 }
 
 function selectbox(){
@@ -49,7 +52,7 @@ function selectbox(){
         if(!checkMatch()){
             setTimeout(()=>{
                 for(i=0;i<selboxes.length;i++){
-                    // Eliminar estilos da
+                    // Desmarcar caixas 
                     let undo=document.getElementById(selid[i]);
                     undo.classList.remove(selboxes[i]);
                     checking=false;
@@ -57,18 +60,25 @@ function selectbox(){
             },1000);
         // Se son iguais
         }else{
+            tofind=tofind-1;
             checking=false;
         }
         e=0;
+        attext();
         return;
     }
 }
-// Se duas caixas xa foron clicadas verificar se hai unha parella
+// Se duas caixas xa foron clicadas verificar se hai unha parella + marcar o checking á true + sumar un intento
 function checkMatch(){
     checking=true;
+    attempts++;
     if(selboxes[0]===selboxes[1]){
         return true;
     }else{
         return false;
     }
+}
+// Liñas de texto para o número de intentos e de parellas por atopar
+function attext(){
+    document.getElementsByClassName("attempts")[0].textContent='Número de intentos: '+attempts+'\nParellas por atopar: '+tofind;
 }
