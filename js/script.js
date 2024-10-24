6bad21c9ad51d18dee674d15885ec26aaf13b32e
mateonation <mattfernandezrivera@gmail.com>
2024-10-24 12:56:22 +0200

    script for selecting rows and columns were implemented

--- js/script.js
+++ js/script.js
@@ -1,36 +1,61 @@
-const total=12;
-var checking=false;
+let total;
+var checking;
 let boxes=[];
 let selboxes=[];
 let selid=[];
-let e=0;
-let attempts=0;
-let tofind=total/2;
-
-// ID posíbel
+let e;
+let attempts;
+let tofind;
 let abc=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 let index=[];
+let shuffled=[];
 
-// Engadir tantos ID ao índice coma sexa posíbel (ata a metade das caixas totais)
-for(let i=0;i<total/2;i++){
-    index+=[abc[i]];
+window.onload=function(){
+    total=12;
+    document.getElementsByClassName('boxes')[0].style.gridTemplateColumns='repeat(4,auto)';
+    document.getElementsByClassName('boxes')[0].style.gridTemplateRows='repeat(3,auto)';
+    generateBoxes();
 }
 
-// Repetir outra vez o índice dentro de si mesmo e mesturar os elementos nel
-index=[...index,...index]
-const shuffled = index.sort((a, b) => 0.5 - Math.random());
 
-// XERAR CAIXAS NO HTML
-for(let i=0;i<total;i++){
-    let div=document.createElement('div');
-    div.classList.add('box');
-    div.addEventListener('click',selectbox);
-    div.id=i;
-    boxes.push(div);
-    document.querySelector('.boxes').append(boxes[i]);
+
+//----------------//
+//----------------//
+//----------------//
+
+
+
+// Xerador de caixas dentro do xogo
+function generateBoxes(){
+    // Engadir tantos ID ao índice coma sexa posíbel (ata a metade das caixas totais)
+    for(let i=0;i<total/2;i++){
+        index[i]=[abc[i]];
+    }
+    // Repetir outra vez o índice dentro de si mesmo e mesturar os elementos nel
+    index=[...index,...index];
+    shuffled = index.sort((a, b) => 0.5 - Math.random());
+    // XERAR CAIXAS COS DATOS OBTIDOS NO HTML
+    for(let i=0;i<total;i++){
+        let div=document.createElement('div');
+        div.classList.add('box');
+        div.addEventListener('click',selectbox);
+        div.id=i;
+        boxes.push(div);
+        document.querySelector('.boxes').append(boxes[i]);
+    }
+    // probas
+    e=0;
+    // nº de intentos totais
+    attempts=0;
+    // nº parellas a atopar
+    tofind=total/2;
+    // checking non activo 
+    checking=false;
+    // xerar texto de intentos acumulados + parellas restantes
     attext();
 }
 
+// Cando se clica enriba dunha caixa
 function selectbox(){
     if(checking){
         return;
@@ -81,4 +106,37 @@ function checkMatch(){
 // Liñas de texto para o número de intentos e de parellas por atopar
 function attext(){
     document.getElementsByClassName("attempts")[0].textContent='Número de intentos: '+attempts+'\nParellas por atopar: '+tofind;
+}
+// Cambiar o número de columnas e filas do xogo
+function selectRC(){
+    let rowsn=document.getElementById('rows-n').value;
+    let columnsn=document.getElementById('columns-n').value;
+    // Obter o número provisional das caixas
+    let totaln=rowsn*columnsn;
+    alert(totaln)
+    // Se o total provisional é par + maior que 1 + numeros de columnas e filas positivos
+    if(totaln%2===0 && totaln>1 && columnsn>0 && rowsn>0){
+        // Desmarcar e borrar todas as caixas reveladas
+        for(i=0;i<total;i++){
+            let del=document.getElementById(i);
+            del.classList.remove(shuffled[i]);
+            del.remove();
+        }
+        // Fixar o total provisional ao total real das caixas
+        total=totaln;
+        // Resetear arrays
+        index=[];
+        shuffled=[];
+        boxes=[];
+        // Aplicar os estilos de filas e columnas
+        document.getElementsByClassName('boxes')[0].style.gridTemplateColumns='repeat('+columnsn+',auto)';
+        document.getElementsByClassName('boxes')[0].style.gridTemplateRows='repeat('+rowsn+',auto)';
+        // Volver a xerar as caixas cos novos parámetros
+        generateBoxes();
+        return;
+    // Se é impar ou menor que 1 => amosar erro e non xerar    
+    }else{
+        alert('erro')
+        return;
+    }
 }
